cb2499095a12d53a4e07666c8a05ce6fd5df4eb4
Mark Locse <fbqp0079@leeds.ac.uk>
2026-02-13 10:35:00 +0000

    dguhihighseifhhifshfhweif

--- session-2/task3/compound_interest.c
+++ session-2/task3/compound_interest.c
@@ -6,6 +6,7 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 
 int main( void ) {
 
@@ -19,6 +20,10 @@ int main( void ) {
      * Implement the formula for compound interest.
      * Print out the final amount and the gain compared to the initial deposit.
      */
+     final_amount = deposit*pow(1+rate,term);
+     gain = final_amount - deposit;
+     printf("Final Amount: %.2f\n", final_amount);
+     printf("Gain: %.2f\n", gain);
     
 
     return 0;
